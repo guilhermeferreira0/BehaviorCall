1730d6315821d6966e0fe0920e4a4bab8c09b9c9
Guilherme Ferreira <guilherme.fsilva23@gmail.com>
2023-09-23 17:13:42 -0300

    concluindo menu

--- frontend/src/components/bars/index.js
+++ frontend/src/components/bars/index.js
@@ -1,42 +1,75 @@
 import React, { useState } from 'react';
-import { Modal, Text, TouchableOpacity, View, Icon, SafeAreaView } from 'react-native';
-import { Octicons  } from '@expo/vector-icons';
+import { Modal, Text, TouchableOpacity, View, SafeAreaView } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import { Octicons, EvilIcons, AntDesign, Feather   } from '@expo/vector-icons';
 
 import styles from './style';
 
 function Bars() {
-  const [ visible, setVisible ] = useState(false);
-  const options = [
-    {
-      title: 'Publicar',
-      icon: 'Teste',
-      action: () => alert('Teste'),
-    }
-  ];
+
+  const  [ visible, setVisible ] = useState(false);
 
   return(
-    <View>
+    <>
       <TouchableOpacity onPress={() => setVisible(true)}>
         <Octicons name="three-bars" size={24} color="black" />
       </TouchableOpacity>
-      <Modal transparent visible={'false'}>
-        <SafeAreaView
-          style={{flex: 1, backgroundColor: 'red'}}
-          onTouchStart={() => setVisible(false)}
+
+      <SafeAreaView>
+        <Modal
+        transparent={true}
+        visible={visible}
+        animationType='fade'
+        onRequestClose={() => setVisible(false)}
         >
-          <View style={styles.bars}>
-            {options.map((op, i) => (
-            <TouchableOpacity
-              key={i}
-              onPress={() => op.action}
-            >
-              <Text>{op.title}</Text>
-              <Icon name={op.icon} size={26} color='#212121'/>
-            </TouchableOpacity>))}
-          </View>
-        </SafeAreaView>
-      </Modal>
-    </View>
+          <SafeAreaView style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.3)'}}>
+            <TouchableOpacity style={{flex: 1}} onPress={() => setVisible(false)}>
+              <Animatable.View
+                style={styles.barNav}
+                animation='slideInLeft'
+                easing='ease'
+              >
+                <TouchableOpacity>
+                  <EvilIcons name="user" size={150} color="black" />
+                </TouchableOpacity>
+
+                <Text style={styles.textUser}>Fulano</Text>
+                <Text style={styles.textDescription}>Help Seeker</Text>
+                <Text style={styles.textDescription}>Lvl. {}</Text>
+
+                <View style={styles.buttons}>
+
+                  <TouchableOpacity style={styles.flexDireRow}>
+                    <AntDesign name="delete" size={24} color="red" />
+                    <Text style={styles.textDel}>Excluir</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={styles.flexDireRow}>
+                    <Feather name="edit" size={24} color="black" />
+                    <Text style={styles.textEdit}>Editar</Text>
+                  </TouchableOpacity>
+
+                </View>
+
+                <TouchableOpacity style={styles.flexDireRow}>
+                <AntDesign name="setting" size={24} color="black" />
+                  <Text style={styles.textSettings}>Configurações</Text>
+                </TouchableOpacity>
+
+              </Animatable.View>
+
+            </TouchableOpacity>
+          </SafeAreaView>
+
+        </Modal>
+
+        {visible ?
+        <View
+        style={styles.overlay}>
+        </View>
+        : null}
+      </SafeAreaView>
+    </>
   );
 }
 
--- frontend/src/components/bars/style.js
+++ frontend/src/components/bars/style.js
@@ -1,10 +1,66 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  nav: {
-    padding: 30,
-    backgroundColor: 'lightblue',
-  }
+  barNav: {
+    width: '50%',
+    height: '100%',
+    padding: 0,
+    backgroundColor: '#2F86A1',
+    alignItems: 'center',
+  },
+
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    width: 'auto',
+    height: 'auto'
+  },
+
+  buttons: {
+    flexDirection: 'row',
+    marginTop: 25,
+    marginBottom: 450,
+    gap: 25
+  },
+
+  flexDireRow: {
+    flexDirection: 'row',
+  },
+
+  textUser: {
+    marginTop: 20,
+    fontWeight: '700',
+    fontSize: 22,
+    textTransform: 'uppercase',
+    color: '#FFFFFF'
+  },
+
+  textDescription: {
+    fontSize: 14,
+    fontWeight: '300',
+    color: '#FFFFFF'
+  },
+
+  textDel: {
+    color: '#EE0000',
+    fontWeight: '600',
+    marginLeft: 8,
+  },
+
+  textEdit: {
+    color: '#FFFFFF',
+    fontWeight: '600',
+    marginLeft: 8,
+  },
+
+  textSettings: {
+    color: '#FFFFFF',
+    fontWeight: '600',
+    marginLeft: 8,
+  },
+
 })
 
 export default styles;
